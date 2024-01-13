af01b7e86eec018ba7d9ea711a7282b83204166e
Arun Singh <arunsingh28aug.as@gmail.com>
2024-01-13 07:59:11 +0530

    remove node

---
+++ index.js
@@ -0,0 +1,157 @@
+Print("File-LLM");
+
+import dotenv from "dotenv";
+dotenv.config();
+import { ChatOpenAI } from "@langchain/openai";
+import { ChatPromptTemplate } from "@langchain/core/prompts";
+import { StringOutputParser } from "@langchain/core/output_parsers";
+import { createStuffDocumentsChain } from "langchain/chains/combine_documents";
+import { Document } from "@langchain/core/documents";
+import { MemoryVectorStore } from "langchain/vectorstores/memory";
+import { CheerioWebBaseLoader } from "langchain/document_loaders/web/cheerio";
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+import { OpenAIEmbeddings } from "@langchain/openai";
+import { PDFLoader } from "langchain/document_loaders/fs/pdf";
+import express from "express";
+import { fileURLToPath } from "url";
+import path, { dirname } from "path";
+import multer from "multer";
+
+async function main(inputPrompt, fileName) {
+  const chatModel = new ChatOpenAI({
+    openAIApiKey: "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",
+  });
+
+  const filePath = path.join(
+    fileURLToPath(import.meta.url),
+    "../pdfs",
+    fileName
+  );
+
+  const loader = new PDFLoader(filePath, { splitPages: false });
+
+  const docs = await loader.load();
+
+  //   const loader = new CheerioWebBaseLoader(
+  //     ["https://powershop-container.s3.amazonaws.com/ArunPratapSinghResume.pdf"]
+  //     // "https://blueorbit.app"
+  //     // "https://www.getmyparking.com/"
+  //   );
+  //   const docs = await loader.load();
+
+  //   const splitter = new RecursiveCharacterTextSplitter();
+  //   const splitDocs = await splitter.splitDocuments(docs);
+
+  const embeddings = new OpenAIEmbeddings({
+    openAIApiKey: "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",
+  });
+
+  //   const vectorstore = await MemoryVectorStore.fromDocuments(
+  //     splitDocs,
+  //     embeddings
+  //   );
+
+  //   const prompt = ChatPromptTemplate.fromMessages([
+  //     [
+  //       "system",
+  //       "You are chatting with an AI. The AI is very smart and will answer any question you ask.",
+  //     ],
+  //     ["user", "{input}"],
+  //   ]);
+
+  const prompt =
+    ChatPromptTemplate.fromTemplate(`Answer the following question based only on the provided context:
+        <context>
+        {context}
+        </context>
+    Question: {input}
+
+`);
+
+  const documentChain = await createStuffDocumentsChain({
+    llm: chatModel,
+    prompt: prompt,
+  });
+
+  // const outputParser = new StringOutputParser();
+  // const chain = prompt.pipe(documentChain).pipe(outputParser);
+
+  const response = await documentChain.invoke({
+    input: inputPrompt,
+    context: [
+      new Document({
+        pageContent: docs[0].pageContent,
+      }),
+    ],
+  });
+
+  Print(response);
+  return response;
+
+  //   const response = await chain.invoke({
+  //     input: "What is human life expectancy in the United States?",
+  //   });
+}
+
+function Print(arg) {
+  console.log(arg);
+}
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get("/", async (req, res) => {
+  return res.sendFile(
+    path.join(dirname(fileURLToPath(import.meta.url)), "index.html")
+  );
+});
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "pdfs/");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+app.post("/upload", upload.single("file"), async (req, res) => {
+  try {
+    if (!req.file) {
+      return res.json({
+        status: false,
+        message: "No file uploaded",
+      });
+    }
+    console.log(req.file);
+    return res.json({
+      status: true,
+      message: "File is uploaded",
+      data: {
+        name: req.file?.originalname,
+        mimetype: req.file?.mimetype,
+        size: req.file?.size,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    return res.send(`Error when trying upload image: ${error}`);
+  }
+});
+
+app.post("/chat", async (req, res) => {
+  const { input, fileName } = req.body;
+  if (!input || !fileName) {
+    return res.json("File is not uploaded!!!");
+  }
+  const response = await main(input, fileName);
+  return res.json(response);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
